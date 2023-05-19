107aba50ce07344d40125b2842894a4a976d8e13
ABDELKARIM <karimelaissaouy1@gmail.com>
2023-05-18 23:37:48 -0700

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-# 5-number_keys.py
-#abdelkarim elaissaouy
-
+#karim
 
 def number_keys(a_dictionary):
-    """Return the number of keys in a dictionary."""
-    return (len(a_dictionary))
+    """returns the number of keys in a dictionary
+    You are not allowed to import any module
+    """
+
+    # this is an alernative
+    # keys = list(a_dictionary.keys())
+    # count = 0
+    # for count in keys:
+    #     count += 1
+    # return count
+
+    return len(a_dictionary)
