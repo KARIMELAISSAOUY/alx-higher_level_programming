8155c8f24d887a84e76d2fb0bbb69d9de5ce5f3c
ABDELKARIM <karimelaissaouy1@gmail.com>
2023-05-30 01:39:35 -0700

    Python - Exceptions 0x05Py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
 import sys
+
+
 def safe_function(fct, *args):
     """Executes a function safely.
 
@@ -9,11 +12,11 @@ def safe_function(fct, *args):
 
     Returns:
         If an error occurs - None.
-        Otherwise - the result of the call to fct.
+        Otherwise -- the result of the call to fct.
     """
     try:
         result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+        return result
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
