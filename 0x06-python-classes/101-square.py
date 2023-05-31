932e831ee8a812aa6b71574027a2e405c8fc6b23
ABDELKARIM <karimelaissaouy1@gmail.com>
2023-05-30 18:43:42 -0700

    0x06 Python - Classes and Objects

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -16,7 +16,7 @@ class Square:
         self.position = position
 
     @property
- def size(self):
+    def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
 
