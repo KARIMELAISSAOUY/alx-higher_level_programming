c293a15c1de31ff99167126c2b01e2e54e054a0d
ABDELKARIM <karimelaissaouy1@gmail.com>
2023-06-06 03:18:56 -0700

    0x08. Python | More Classes and Objects

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+
 if len(sys.argv) != 2:
     print('Usage: nqueens N')
     sys.exit(1)
@@ -15,11 +16,13 @@ if N < 4:
     print('N must be at least 4')
     sys.exit(1)
 
+
 def is_safe(board, row, col):
     for i in range(col):
         if board[i] == row or board[i] == row - (col - i) or board[i] == row + (col - i):
             return False
     return True
+
 
 def solve(board, col, solutions):
     if col == N:
@@ -29,6 +32,7 @@ def solve(board, col, solutions):
         if is_safe(board, row, col):
             board[col] = row
             solve(board, col + 1, solutions)
+
 
 solutions = []
 solve([0] * N, 0, solutions)
