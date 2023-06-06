7cc445ecfd4c2d362137697d78e177fa773c1248
ABDELKARIM <karimelaissaouy1@gmail.com>
2023-06-05 18:59:51 -0700

    0x08-python-more_classes

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import sys
+
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
+    sys.exit(1)
+
+try:
+    N = int(sys.argv[1])
+except ValueError:
+    print('N must be a number')
+    sys.exit(1)
+
+if N < 4:
+    print('N must be at least 4')
+    sys.exit(1)
+
+def is_safe(board, row, col):
+    for i in range(col):
+        if board[i] == row or board[i] == row - (col - i) or board[i] == row + (col - i):
+            return False
+    return True
+
+def solve(board, col, solutions):
+    if col == N:
+        solutions.append(board[:])
+        return
+    for row in range(N):
+        if is_safe(board, row, col):
+            board[col] = row
+            solve(board, col + 1, solutions)
+
+solutions = []
+solve([0] * N, 0, solutions)
+
+for solution in solutions:
+    print([[row, solution[row]] for row in range(N)])
