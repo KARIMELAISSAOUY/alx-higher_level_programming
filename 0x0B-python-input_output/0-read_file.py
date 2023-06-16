662bf25529a5ad1454f826d414a6718383e48aa1
ABDELKARIM <karimelaissaouy1@gmail.com>
2023-06-15 17:17:01 -0700

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 '''
-Module to Read and print contents of a File
+Module to read And print contents of a file
 '''
 
 
 def read_file(filename=""):
-    '''Reads File and prints contents'''
+    ''' Reads file and prints contents '''
     with open(filename) as open_file:
         contents = open_file.read()
     print(contents, end="")
+
 
