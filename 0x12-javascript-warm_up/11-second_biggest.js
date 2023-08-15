60ce489aaa0e1780a26f97061894db6321ea3917
ABDELKARIM <karimelaissaouy1@gmail.com>
2023-08-15 05:53:28 -0700

    0x12-javascript-warm_up alx

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+
+if (process.argv.length <= 2) {
+  console.log(0);
+} else if (process.argv.length === 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  const numbers = process.argv.slice(2).map(Number);
+  numbers.sort((a, b) => b - a);
+  console.log(numbers[1]);
 }
