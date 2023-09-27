d5406543f8d13603e950039b2fbcd14fc6a1ee9c
ABDELKARIM ELAISSAOUY <118484209+KARIMELAISSAOUY@users.noreply.github.com>
2023-09-27 01:47:34 +0100

    0x14-javascript-web_scraping
    0x14-javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+  display the status code of a GET request
+  */
+const myArgs = process.argv.slice(2);
+const request = require('request');
+request(myArgs[0], function (error, response, body) {
+  if (!error && response) {
+    console.log('code:', response.statusCode);
+  }
+});
